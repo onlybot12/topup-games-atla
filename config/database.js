07bcca92c6373634dc65863c546747b66b6f5df9
Maulana14 <116287549+onlybot12@users.noreply.github.com>
2026-01-26 09:26:16 +0700

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://Maulanaa:5q1PrEZUUJkY4ioF@cluster0.rgcpg7g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            dbName: 'raystore'
+        });
+        console.log('MongoDB Connected');
+    } catch (error) {
+        console.error('MongoDB Connection Error:', error.message);
+        process.exit(1);
+    }
+};
+
+mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB Disconnected');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB Error:', err);
+});
+
+module.exports = connectDB;
