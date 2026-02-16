9f6f78a43724fa46cf94e286517042bf249895d2
Maulana14 <116287549+onlybot12@users.noreply.github.com>
2026-02-17 04:10:41 +0700

    Update Config.js

--- models/Config.js
+++ models/Config.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
 const ConfigSchema = new mongoose.Schema({
-    key: { type: String, unique: true }, // Contoh: 'qris_settings'
-    admin_fee: { type: Number, default: 700 }, // Biaya Rp (Contoh: 700)
-    tax_percent: { type: Number, default: 1.4 } // Biaya % MDR (Contoh: 1.4)
+    key: { type: String, unique: true },
+    admin_fee: { type: Number, default: 700 },
+    tax_percent: { type: Number, default: 1.4 },
+    // SETTING JUDUL SECTION
+    title_popular: { type: String, default: "POPULER SEKARANG!" },
+    title_flash_sale: { type: String, default: "Flash Sale" },
+    // SETTING SOSMED & FOOTER
+    wa_link: { type: String, default: "" },
+    ig_link: { type: String, default: "" },
+    fb_link: { type: String, default: "" },
+    tiktok_link: { type: String, default: "" }
 });
 
 module.exports = mongoose.model('Config', ConfigSchema);
