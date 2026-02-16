45aa6d2daad2f7ab7415492f10acbfafc7b8907a
Maulana14 <116287549+onlybot12@users.noreply.github.com>
2026-02-17 04:08:45 +0700

    Update Brand.js

--- models/Brand.js
+++ models/Brand.js
@@ -3,19 +3,17 @@ const mongoose = require('mongoose');
 const BrandSchema = new mongoose.Schema({
     name: { type: String, required: true },
     slug: { type: String, unique: true, required: true },
-    category: { type: String, required: true }, // Contoh: Games, Pulsa, dll
+    category: { type: String, required: true },
     icon_url: { type: String, required: true },
     status: { type: String, enum: ['active', 'inactive'], default: 'active' },
-    // Konfigurasi Form sesuai Foto
+    is_popular: { type: Boolean, default: false }, // FIELD BARU
     form_config: {
         target_label: { type: String, default: 'User ID' },
-        target_type: { type: String, default: 'text' }, // text, number, email
+        target_type: { type: String, default: 'text' },
         has_server: { type: Boolean, default: false },
         server_label: { type: String, default: 'Server ID' },
-        server_type: { type: String, default: 'text' },
-        is_dropdown: { type: Boolean, default: false }
+        server_type: { type: String, default: 'text' }
     },
-    // Menghubungkan ke ID Layanan dari database Service
     services: [{ type: String }] 
 });
 
