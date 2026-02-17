902d9cef4e55faff93d39b5720fa6a65f2d58478
Maulana14 <116287549+onlybot12@users.noreply.github.com>
2026-02-17 13:16:31 +0700

    Update Config.js

--- models/Config.js
+++ models/Config.js
@@ -4,14 +4,17 @@ const ConfigSchema = new mongoose.Schema({
     key: { type: String, unique: true },
     admin_fee: { type: Number, default: 700 },
     tax_percent: { type: Number, default: 1.4 },
-    // SETTING JUDUL SECTION
     title_popular: { type: String, default: "POPULER SEKARANG!" },
-    title_flash_sale: { type: String, default: "Flash Sale" },
-    // SETTING SOSMED & FOOTER
+    title_flash_sale: { type: String, default: "PASTI TER-MURAAHH" },
     wa_link: { type: String, default: "" },
     ig_link: { type: String, default: "" },
-    fb_link: { type: String, default: "" },
-    tiktok_link: { type: String, default: "" }
+    tiktok_link: { type: String, default: "" },
+    
+    // --- TAMBAHAN SEO ---
+    shop_name: { type: String, default: "Lana Store" },
+    meta_description: { type: String, default: "Topup Game Tercepat & Termurah di Indonesia" },
+    meta_keywords: { type: String, default: "topup game, diamond ml, ff murah, vouchers" },
+    logo_url: { type: String, default: "" }
 });
 
 module.exports = mongoose.model('Config', ConfigSchema);
