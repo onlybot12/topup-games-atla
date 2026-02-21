2b31763c67fc42f31560f4b94689a863cdf6743d
Maulana14 <116287549+onlybot12@users.noreply.github.com>
2026-02-22 01:14:14 +0700

    Rename UserSchema to AdminSchema in AdminLog
    Updated the schema name to AdminSchema for consistency.

--- models/AdminLog.js
+++ models/AdminLog.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+// Nama schema diubah agar konsisten dengan AdminLog
+const AdminSchema = new mongoose.Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true }
 });
 
-module.exports = mongoose.model('AdminLog', UserSchema);
+// Export dengan nama model AdminLog
+module.exports = mongoose.model('AdminLog', AdminSchema);
