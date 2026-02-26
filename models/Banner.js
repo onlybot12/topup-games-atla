e0621f26fe8e9892ba7b5b2318cd35d3fef09ae5
Maulana14 <116287549+onlybot12@users.noreply.github.com>
2026-02-26 23:02:43 +0700

    Update Banner.js

--- models/Banner.js
+++ models/Banner.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const BannerSchema = new mongoose.Schema({
     image_url: String,
     title: String,
-    target_link: String // Link ke game saat diklik
+    target_link: String 
 });
 module.exports = mongoose.model('Banner', BannerSchema);
