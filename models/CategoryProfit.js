f144da79718b0da03a744600e13ef7d56e662416
Maulana14 <116287549+onlybot12@users.noreply.github.com>
2026-02-26 23:04:25 +0700

    Update CategoryProfit.js

--- models/CategoryProfit.js
+++ models/CategoryProfit.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const CategoryProfitSchema = new mongoose.Schema({
-    category_name: { type: String, unique: true, required: true }, // Nama kategori dari Atlantic
-    type: { type: String, enum: ['percentage', 'fixed'], default: 'percentage' }, // Persen atau Rupiah
-    value: { type: Number, default: 0 } // Angkanya
+    category_name: { type: String, unique: true, required: true }, 
+    type: { type: String, enum: ['percentage', 'fixed'], default: 'percentage' }, 
+    value: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('CategoryProfit', CategoryProfitSchema);
