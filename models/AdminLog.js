4bc53cde3a9526b5cb4a5efa7f013083cbb3b672
Maulana14 <116287549+onlybot12@users.noreply.github.com>
2026-02-26 23:02:23 +0700

    Update AdminLog.js

--- models/AdminLog.js
+++ models/AdminLog.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
-
-// Nama schema diubah agar konsisten dengan AdminLog
 const AdminSchema = new mongoose.Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true }
 });
 
-// Export dengan nama model AdminLog
 module.exports = mongoose.model('AdminLog', AdminSchema);
